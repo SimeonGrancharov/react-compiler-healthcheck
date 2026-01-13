9aa351aed24e86105d0f98ee7f5acab975206855
simeon.grancharov <simeon.grancharov@cubbystorage.com>
2026-01-13 11:10:34 +0200

    Replace tsup with esbuild directly

---
+++ dist/reporters/console.d.ts
@@ -0,0 +1,4 @@
+import type { HealthcheckResult } from "../core/types";
+export declare function formatConsoleReport(result: HealthcheckResult): string;
+export declare function printConsoleReport(result: HealthcheckResult): void;
+//# sourceMappingURL=console.d.ts.map
